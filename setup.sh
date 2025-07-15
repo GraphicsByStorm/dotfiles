67f1a4932cb17cdb25c80f2cfc5ce7bdc0fc35a5
GraphicsByStorm <89564679+GraphicsByStorm@users.noreply.github.com>
2025-07-15 00:41:04 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
+set -e  # Exit on error
+
 option=$1
 theme=$2
 
 shopt -s nocasematch
 case "$option" in
-    "all" )     ./setup-scripts/packages.sh && ./setup-scripts/themes.sh && ./setup-scripts/rices.sh $theme ;; # $theme is theme name
-    "packages" )  ./setup-scripts/packages.sh ;; 
-    #"pacman" )  ./setup-scripts/pacman-packages.sh ;; 
-    #"aur" )     ./setup-scripts/aur-packages.sh ;; 
-    #"dm" )      ./setup-scripts/dm.sh ;; 
-    "themes" )  ./setup-scripts/themes.sh ;; # Installs themes, icons, fonts, wallpapers and scripts
-    "rice" )    ./setup-scripts/rices.sh $theme ;; # $theme is theme name
-
-    *) echo "[ERROR]: no install flag with name \"$option\" found" && exit 1 ;;
+    "all" )
+        ./setup-scripts/pacman-packages.sh
+        ./setup-scripts/aur-packages.sh
+        ./setup-scripts/themes.sh
+        ./setup-scripts/rices.sh "$theme"
+        ;;
+    "packages" )
+        ./setup-scripts/pacman-packages.sh
+        ./setup-scripts/aur-packages.sh
+        ;;
+    "themes" )
+        ./setup-scripts/themes.sh
+        ;;
+    "rice" )
+        ./setup-scripts/rices.sh "$theme"
+        ;;
+    *)
+        echo "[ERROR]: No install flag with name \"$option\" found"
+        exit 1
+        ;;
 esac
-
